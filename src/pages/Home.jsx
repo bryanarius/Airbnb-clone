a507708fa8aa07b8f182314392e602b7751d2555
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-08 03:12:49 -0400

    Footer and new section
    - Updates to the footer section
    - Added section to home page
    - CSS changes
    - Image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Photo from '../../public/images/photo-grid.png'
 import Card from "../components/Card"
+import Woman from '../../public/images/woman.png'
 import data from "../data"
 
 export default function Home() {
@@ -23,8 +24,20 @@ export default function Home() {
                 one-of-a-kind hosts—all without leaving home.</p> 
         </section>
 
-        <section className="card-list">
+        <section className="woman">
+        <img src={Woman} className="woman--photo" />
+        <div>
+            <h3>Try Hosting</h3>
+            <p>earn up to $2,755/month by sharing your space in Jacksonville</p>
+            <button className="dark--button">Learn More</button>
+        </div>
+        </section>
+
+        <section >
+            <h3>Just booked in the last 24 hours</h3>
+            <div className="card-list">
                 {cards}
+            </div>
          </section>
     </div>
     
