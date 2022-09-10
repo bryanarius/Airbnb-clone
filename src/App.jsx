1c6578f1f2cc1e18a3e39e9cce9ed2af88ea9ccf
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-10 03:13:48 -0400

    Login/Signup
    - Added UI Material dependency
    - Created Signup page
    - Created Login page
    - Route path to login and sign up page fixed
    - CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from "react"
 import {Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import LogIn from "./pages/LogIn"
 import Footer from './components/Footer'
 
 export default function App() {
@@ -13,6 +15,9 @@ export default function App() {
             <Navbar />
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<LogIn />} />
+                
             </Routes>
 
             <Footer />
